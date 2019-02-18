f7e94fa0a22c7f570be5b258d829dba432222e45
wk-j <somnuk.wk@gmail.com>
2019-02-19 04:55:59 +0700

    Fixed MyConsole

--- tests/MyConcole/Program.cs
+++ tests/MyConcole/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MyConcole {
     class Program {
         static async Task Main(string[] args) {
             await new HostBuilder().ConfigureServices((hostContext, services) => {
-                services.AddHostedService<ConsoleService>();
+                services.AddHostedService<ConsoleService.ConsoleService>();
             })
             .RunConsoleAsync();
         }
